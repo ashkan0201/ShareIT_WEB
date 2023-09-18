95a5e29d740447a430715d0f66f81e549b00772b
ashkan0201 <ashkan02011@gmial.com>
2023-09-19 01:16:14 +0330

    Added format_size func

--- app.py
+++ app.py
@@ -24,3 +24,14 @@ def sql_code(path):
 con,cur = sql_code("database/data_of_files.db")
 cur.execute("DELETE FROM data")
 con.commit()
+
+# A function that calculates the volume
+def format_size(size):
+    units = ["B", "KB", "MB", "GB"]
+    index = 0
+    while size >= 1024 and index < len(units) - 1:
+        size /= 1024
+        index += 1
+    size = round(size, 2)
+    formatted_size = f"{size} {units[index]}"
+    return formatted_size
